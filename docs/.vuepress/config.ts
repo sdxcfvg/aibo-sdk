afedcbfd5420d1b3e13f24eecf4277e7f573fad2
草莓奶昔 <lmt2818088@gmail.com>
2022-07-30 20:56:54 +0800

    chore: update vuepress base url

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,7 @@ export default defineUserConfig({
   description: packageJson.description,
 
   // 将部署站点的基本 URL github上用的前缀  eg:'/project name/'
-  base: `/TongBan-monitor/${packageJson.name}/`,
+  base: `/tongban-monitor/${packageJson.name}/tree/gh-pages/`,
 
   theme: defaultTheme({
     navbar: [
